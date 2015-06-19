81441930f3ecfd908920ebf01821de8e77f2516c
Dominick Stuck <das66@psu.edu>
2015-06-19 10:33:20 -0400

    Modified the Models file changing DateTimeField values

--- models.py
+++ models.py
@@ -7,22 +7,25 @@ import datetime
 
 # Create your models here.
 class Locker(models.Model):
-    pass
+    locker = models.CharField(max_length=250)
+    user_id = models.CharField(max_length=250)
 # Model used for the actual Submission of the form
 # Needs to include the locer name, Submission timestamp,
 # the data that is on the form and then it is needed to be returned readable
 class Submission(models.Model):
     locker = models.ForeignKey(Locker)
-    timestamp = models.DateTimeField()
-    data = models.CharField()
+    timestamp = models.DateTimeField(auto_now=False,
+        auto_now_add=True
+        )
+    data = models.CharField(max_length=250)
 
 
     def data_dict(self):
-        return data.to_dict()
+        return self.to_dict()
 
 
     def to_dict(self):
-        return model_to_dict(Submission)
+        return model_to_dict(self)
 
 
 # Model to show the owner/user_id of an added locker
