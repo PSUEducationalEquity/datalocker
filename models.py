a3f47e2915e4261026690cfa1fddd4a5254b673f
Dominick Stuck <das66@psu.edu>
2015-06-19 09:37:00 -0400

    Modified the models. Added fields to Submission and LockerUser classes

--- models.py
+++ models.py
@@ -1,20 +1,31 @@
 from django.db import models
-from django.froms.models import model_to_dict
+from django.forms.models import model_to_dict
 from django.utils import timezone
 
 import datetime
 
 
 # Create your models here.
-# Model used for the actual submission of the form
+class Locker(models.Model):
+    pass
+# Model used for the actual Submission of the form
 # Needs to include the locer name, Submission timestamp,
 # the data that is on the form and then it is needed to be returned readable
 class Submission(models.Model):
-    locker = models.ForeignKey('Locker')
-    timestamp = models.DateTimeField(auto_add_now=True)
-    data = models.Text()
-    model_to_dict(Submission)
+    locker = models.ForeignKey(Locker)
+    timestamp = models.DateTimeField()
+    data = models.CharField()
 
+
+    def data_dict(self):
+        return data.to_dict()
+
+
+    def to_dict(self):
+        return model_to_dict(Submission)
+
+
+# Model to show the owner/user_id of an added locker
 class LockerUser(models.Model):
-    locker = models.ForeignKey('Locker')
-    user_id = models.ForeignKey('Locker')
+    locker = models.ForeignKey(Locker)
+    user_id = models.ForeignKey(Locker)
