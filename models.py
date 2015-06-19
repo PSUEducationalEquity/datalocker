c1569b66b0e6b6cc121530ddd8a828130c18164d
Dominick Stuck <das66@psu.edu>
2015-06-19 07:48:53 -0400

    Added basic Submission and LockerUser Classes

--- models.py
+++ models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.froms.models import model_to_dict
+from django.utils import timezone
+
+import datetime
+
 
 # Create your models here.
+# Model used for the actual submission of the form
+# Needs to include the locer name, Submission timestamp,
+# the data that is on the form and then it is needed to be returned readable
+class Submission(models.Model):
+    locker = models.ForeignKey('Locker')
+    timestamp = models.DateTimeField(auto_add_now=True)
+    data = models.Text()
+    model_to_dict(Submission)
+
+class LockerUser(models.Model):
+    locker = models.ForeignKey('Locker')
+    user_id = models.ForeignKey('Locker')
