53445e79aac1558640ef4057af0357f0d81338da
Dominick Stuck <das66@psu.edu>
2015-06-23 11:57:18 -0400

    Added url line to switch to the class view styling

--- urls.py
+++ urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from datalocker import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.LockerView.as_view(), name='index'),
+
 )
