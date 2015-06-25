d660e7bb4ad1f48910775b333df88ad512e0b174
Dominick Stuck <das66@psu.edu>
2015-06-25 15:46:26 -0400

    Modifited the urls page to correctly load every page

--- urls.py
+++ urls.py
@@ -4,12 +4,14 @@ from datalocker import views
 urlpatterns = patterns('',
     url(r'^$', views.LockerListView.as_view(), name='index'),
     url(r'^(?P<locker_id>[0-9]+)/submissions$',
-    	views.LockerSubmissionView.as_view(),
-    	name='submissions_list'
+    	views.LockerSubmissionView.as_view(
+        context_object_name='my_submission_list'),
+        name='submissions_list',
     	),
     url(r'^(?P<locker_id>[0-9]+)/submissions/(?P<submission_id>[0-9]+)/view$',
-    	views.SubmissionView.as_view(),
-    	name='submission_view'
+    	views.SubmissionView.as_view(
+        context_object_name='submission_view'),
+    	name='submissions_view'
     	),
 )
 
