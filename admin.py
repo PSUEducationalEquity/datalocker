9f637d968cdac3a43209097424fbad67d6579ca7
Dominick Stuck <das66@psu.edu>
2015-07-16 09:19:00 -0400

    Added LockerSetting model to the admin site for easier control over the database records

--- admin.py
+++ admin.py
@@ -4,7 +4,7 @@ from django.contrib.admin import AdminSite
 from django.contrib.auth.admin import GroupAdmin, UserAdmin
 from django.contrib.auth.models import Group, User
 
-from .models import Locker, Submission
+from .models import Locker, LockerSetting, Submission
 
 
 class DataLockerAdminSite(AdminSite):
@@ -25,8 +25,15 @@ class LockerAdmin(admin.ModelAdmin):
     list_display = ['id','form_url','form_identifier','owner','name','create_timestamp','archive_timestamp']
 
 
+
+
+class SettingAdmin(admin.ModelAdmin):
+    list_display = ['category','setting','setting_identifier','value','locker']
+
+
 admin_site = DataLockerAdminSite(name = 'datalockeradmin')
 admin_site.register(Group, GroupAdmin)
 admin_site.register(Locker, LockerAdmin)
 admin_site.register(Submission, SubmissionAdmin)
+admin_site.register(LockerSetting, SettingAdmin)
 admin_site.register(User, UserAdmin)
