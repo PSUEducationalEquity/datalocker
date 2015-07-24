80e34f4aa8d264f0a7f5c0449c1df8ccf62c9f99
unknown <hay110@EEQUP033.equity.psu.edu>
2015-07-24 10:46:17 -0400

    made changes to make the code to make it more simple

--- management/commands/purge_submission.py
+++ management/commands/purge_submission.py
@@ -6,24 +6,22 @@ Shell script to delete the purged submissions after 3 days
 from django.core.management.base import NoArgsCommand, CommandError
 from django.core.urlresolvers import reverse
 from django.utils import timezone
-
-# from datatime import timedelta
-import datetime
 from datalocker.models import Locker, Submission
+import datetime
 
 class Command(NoArgsCommand):
     help = 'Deletes the purged submissions after 3 days'
 
     def handle(self, *args, **options):
-        submissions = Submission.objects.all()
-        duration = datetime.timedelta(days=3)
         now = datetime.datetime.now(timezone.utc)
+        oldest_date = now - datetime.timedelta(days=3)
+        submissions = Submission.objects.filter(deleted__lt=oldest_date)        
     	for submission in submissions:     
-            if submission.deleted != None:
-                deleted_time = submission.deleted 
-                time = now - deleted_time
-                if time >= duration:
-                    submission.delete()       
+            # if submission.deleted != None:
+            #     deleted_time = submission.deleted 
+            #     time = now - deleted_time
+            #     if time >= oldest_date:
+            submission.delete()       
 
 
 
