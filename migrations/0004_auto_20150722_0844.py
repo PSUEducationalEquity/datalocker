c2e272de212138b0fcb0bd563e6b17f1fe07233b
Dominick Stuck <das66@psu.edu>
2015-07-29 08:26:42 -0400

    Fixed Migration Commenting issues

--- migrations/0004_auto_20150722_0844.py
+++ migrations/0004_auto_20150722_0844.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 ### Copyright 2015 The Pennsylvania State University. Office of the Vice Provost for Educational Equity. All Rights Reserved. ###
+
 from __future__ import unicode_literals
 
 from django.db import models, migrations
