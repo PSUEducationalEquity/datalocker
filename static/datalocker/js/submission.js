2fc65f111b50f298f4bcf5e5d3038347c2c90f74
unknown <hay110@EEQUP033.equity.psu.edu>
2015-07-29 13:40:17 -0400

    fixed the indenting and the roles of the buttons

--- static/datalocker/js/submission.js
+++ static/datalocker/js/submission.js
@@ -3,8 +3,8 @@
 (function (Submission, $, undefined)
 {
     // the AJAX objects that handles server communication
-    Submission.addRequest;
     Submission.deleteRequest;
+    Submission.undeleteRequest;
 
     /**
      * Delete's and Undelete's submissions
@@ -12,64 +12,80 @@
 
 Submission.delete = function (locker_id, id)
 {
-    // submit the request
-    Submission.addRequest = $.ajax({
+    // submits the request to delete the submission
+    Submission.deleteRequest = $.ajax({
         url: '/datalocker/'+locker_id+'/submissions/' + id + '/delete_submission',
         type: "post",
-        data: {
+        data:{
             id: id,
             csrfmiddlewaretoken: $("#delete_undelete_form").find(
                "input[name='csrfmiddlewaretoken']").val()
             },
-        success: function(data){
+        success: function(data) {
+            // deletes the submission and adds the class "deleted"
             $("#submission-list tr[data-id='" + id +"']").addClass('deleted');
-            $("#submission-list tr[data-id='" + id +"'] button[role='delete']").html(
+            $("#submission-list tr[data-id='" + id +"'] button[role='delete-submission']").html(
                 'Undelete');
+            Submission.deleteRequest = null;
+            },
+        error: function(jqXHR, textStatus, errorThrown) {
+                console.error(
+                    "Submission.delete in Submission.js AJAX error: "
+                );
+            }
 
-        }
-    });
-}
+        });
+    Submission.deleteRequest = null;
+    }
 
 
 Submission.undelete = function (locker_id, id)
 {
-    // submit the request
-    Submission.deleteRequest = $.ajax({
+    // submits the request to undelete the submission
+    Submission.undeleteRequest = $.ajax({
         url: '/datalocker/'+locker_id+'/submissions/' + id + '/undelete_submission',
         type: "post",
-        data: {
+        data:{
             id : id,
             csrfmiddlewaretoken: $("#delete_undelete_form").find(
                 "input[name='csrfmiddlewaretoken']").val()
-              },
-        success: function(data){
+            },
+        success: function(data) {
+            // undeletes the submission and removes the class "deleted"
             $("#submission-list tr[data-id='"+id +"']").removeClass('deleted');
-            $("#submission-list tr[data-id='"+id +"'] button[role='delete']").html(
+            $("#submission-list tr[data-id='"+id +"'] button[role='delete-submission']").html(
                 'Delete');
+             Submission.undeleteRequest = null;
+            },
+        error: function(jqXHR) {
+                console.error(
+                    "Submission.undelete in Submission.js AJAX error: "
+                );
+            }
 
-        }
-    });
-}
+        });
+    Submission.undeleteRequest = null;
+    }
+
 }( window.Submission = window.Submission || {}, jQuery));
 
 
-$(document).ready(function (){
-    $("[role='delete']").on("click", function (event){
+$(document).ready(function()
+{
+    $("#submission-list").on("click","button[role='delete-submission']", function (event) {
         event.preventDefault();
         var id = $(this).closest("tr").attr("data-id");
         var locker_id = $(this).closest("tr").attr("locker-id");
         if ($(this).html() == "Delete") {
-            Submission.delete(locker_id, id );
+            Submission.delete(locker_id, id);
             $(this).html('Undelete');
-        } else {        
+        } else {
             Submission.undelete(locker_id, id);
-             $(this).html('Delete');
+            $(this).html('Delete');
         }
-     });
-
+    });
     $(".onoffswitch-checkbox").on("click", function (event) {
-        var id = $(this).closest("tr").attr("data-id");
-      $(".delete-button").toggle();
-      $(".deleted").toggle();
+        $(".delete-submission").toggle();
+        $(".deleted").toggle();
     });
 });
