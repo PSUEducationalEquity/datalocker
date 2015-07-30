76a7e9c1db671f2a7125ef7e19ab90d98dfdd524
Dominick Stuck <das66@psu.edu>
2015-07-30 09:47:30 -0400

    Added a cookie function and variable to save the show/hide archived lockers setting

--- static/datalocker/js/locker.js
+++ static/datalocker/js/locker.js
@@ -250,12 +250,38 @@ $(document).ready(function () {
             $('.is-archived').show();
             $(this).addClass('button-archived-showhide is-active');
             $(this).html('Hide Archived Lockers');
+            document.cookie="show/hide=show";
         } else {
             $('.is-archived').hide();
             $(this).removeClass('button-archived-showhide is-active');
             $(this).html('Show Archived Lockers');
+            document.cookie="show/hide=hide"
         }
     });
+
+    var showHide = getCookie("show/hide");
+    if (showHide == "show") {
+        $('.is-archived').show();
+        $(".button-archived-showhide").addClass('button-archived-showhide is-active');
+        $(".button-archived-showhide").html('Hide Archived Lockers');
+    }
+    else {
+        $('.is-archived').hide();
+        $(".button-archived-showhide").removeClass('button-archived-showhide is-active');
+        $(".button-archived-showhide").html('Show Archived Lockers');
+    }
+
+    // Taken from w3 schools to retireve a cookie value
+    function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
 
     // Enables tablesorter JS on the tablesorter tables
     $('.tablesorter').tablesorter();
