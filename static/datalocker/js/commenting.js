44160217a98e7fd181b1d8b174d0c55f7b5bf66e
Dominick Stuck <das66@psu.edu>
2015-08-06 14:32:21 -0400

    Moved all of the add code out of the document.ready and put it inside a function

--- static/datalocker/js/commenting.js
+++ static/datalocker/js/commenting.js
@@ -5,24 +5,29 @@
     Comment.addRequest;
     Comment.dataRequest;
 
+    Comment.add = function () {
+        var comment = $("textarea#comment-text").val();
+        $("#comment-feed-list").append(
+            $("<li />").attr("class","comment-li"
+                ).append(
+                    $("<div />"
+                        ).append(
+                            $("<span />").attr("class","user-image"
+                        )).append(
+                            $("<div />").attr("class","single-comment-text"
+                        ).append(comment
+                    ).append(
+                        $("<div />").attr("class","single-comment-options pull-right"
+                    ).append($("<a />").attr("href","#").html("Reply"))))));
+        $("textarea#comment-text").val('');
+    }
+
 }( window.Comment = window.Comment || {}, jQuery));
 
 
 $(document).ready(function() {
     //Appends the text from the box to the commenting feed
     $("button[role='add-comment']").on("click", function (event) {
-        var comment = $("textarea#comment-text").val();
-        $("#comment-feed-list").append(
-                $("<li />").attr("class","comment-li"
-                ).append(
-                    $("<span />").attr("class","user-image"
-                        )
-                ).append(
-                    $("<div />").attr("class","single-comment-text"
-                        ).append(comment
-                ).append(
-                    $("<div />").attr("class","single-comment-options pull-right"
-                        ).append($("<a />").attr("href","#").html("Reply")))));
-        $("textarea#comment-text").val('');
+        Comment.add();
     });
 });
