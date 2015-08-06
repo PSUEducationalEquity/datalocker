7c21eef6f9013c1ddd6367bd45f1b0e56ebf9b7a
Dominick Stuck <das66@psu.edu>
2015-08-06 11:26:06 -0400

    Created the commenting.js file

---
+++ static/datalocker/js/commenting.js
@@ -0,0 +1,5 @@
+/*! Copyright 2015 The Pennsylvania State University. Office of the Vice Provost for Educational Equity. All Rights Reserved. */
+(function (Comment, $, undefined)
+{
+
+}( window.Comment = window.Comment || {}, jQuery));
