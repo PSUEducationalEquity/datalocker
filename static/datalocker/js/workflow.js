5385747ee8fe1cbfcc96a21661b9bda3f2614402
Hunter Yohn <hay110@psu.edu>
2015-08-12 08:33:29 -0400

    Added more javascript for the change of the workflow state,added copyright to the top of the page

--- static/datalocker/js/workflow.js
+++ static/datalocker/js/workflow.js
@@ -1,4 +1,54 @@
 
+/*! Copyright 2015 The Pennsylvania State University. Office of the Vice Provost for Educational Equity. All Rights Reserved. */
+
+
+  /**
+   * Changes the workflow state for the submisison 
+   *
+   * @return     void
+   * @author     Hunter Yohn  <hay110@psu.edu>
+  */
+
+
+(function (Submission, $, undefined)
+{
+    // the AJAX objects that handles server communication
+    Submission.workflowRequest;
+
+
+    Submission.changeWorkflowState = function () {        
+        var changeWorkflowStateUrl = $("#dialog-edit-users form").attr("action");
+        Submission.workflowRequest = $.ajax({
+            url: changeWorkflowStateUrl,
+            type: "post",
+            data: {
+                email: email,
+                csrfmiddlewaretoken: $("#dialog-edit-users").find(
+                    "input[name='csrfmiddlewaretoken']").val()
+                }
+        });
+
+        // callback handler: success
+        Submission.workflowRequest.done(function (response, textStatus, jqXHR) {
+            if ($("#no-users-message").length){
+                $("#no-users-message").remove();
+            }
+            $("#existing-users").append(Locker._build_user_list_entry(response));
+            $("#email").val("");
+            $("#email").focus();
+            Submission.workflowRequest = null;
+        });
+
+        // callback handler: failure
+        Submission.workflowRequest.fail(function (jqXHR, errorThrown) {
+            if (errorThrown != "abort") {
+                console.error("Locker.add in Locker.js AJAX error");
+            }
+            Submission.workflowRequest = null;
+        });
+    }
+}( window.Locker = window.Locker || {}, jQuery));
+
 $(document).ready(function () {
     $('#locker-options').hide();
     $('select[id=states]').change( function(){
