6b4f9b32bcd82ec030915f6c52b2569d645b6f1b
Dominick <das66@psu.edu>
2015-08-12 13:50:34 -0400

    Started the class to add the color pool to

--- helpers.py
+++ helpers.py
@@ -1,1 +1,7 @@
 #started a helpers.py file, not sure what to put into here yet.
+from .models import User
+
+
+class UserColorHelper():
+    #Need to figure out how to use the css combinations here to assign
+    #Them to a User for the logged in session
