7681148a03b5ed6afe7ec845718ad01535b6b814
Dominick <das66@psu.edu>
2015-08-12 14:07:21 -0400

    Added a possible user line

--- helpers.py
+++ helpers.py
@@ -5,3 +5,5 @@ from .models import User
 class UserColorHelper():
     #Need to figure out how to use the css combinations here to assign
     #Them to a User for the logged in session
+    user = request.POST.get('user')
+
