1ee897b3bc18a78c0cce037f8ef654ea0468a274
Dominick Stuck <das66@psu.edu>
2015-08-21 10:58:19 -0400

    Searches the css file, searches each line for a class name, then it tears off the period and drops everything after the space and appends it to a python list

--- helpers.py
+++ helpers.py
@@ -8,14 +8,18 @@ CSS_FILE = os.path.join(BASE_DIR, 'static', 'datalocker', 'css', 'user_colors.cs
 
 
 class UserColorHelper():
-    #Need to figure out how to use the css combinations here to assign
-    #Them to a User for the logged in session
     def list_of_avaiable_colors():
-        with open(CSS_FILE, 'r') as file:
-            for line in file:
-                print line
-
+        """
+        Searches the css file and assigns all of the classes
+        defined into a list for use in a session element
+        """
         avail_colors = []
+        with open(CSS_FILE, 'r') as cfile:
+            for line in cfile:
+                if line[:1] == '.':
+                    name = line.split()[0][1:]
+                    avail_colors.append(name)
+        return avail_colors
 
     def color_user_lookup():
         return ""
