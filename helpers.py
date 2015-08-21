50b3dfd491559995c742e7447fd5a47b8503094f
Dominick Stuck <das66@psu.edu>
2015-08-21 11:18:06 -0400

    Added a try block to log if the required css page isn't existing. However the function still returns empty values for now

--- helpers.py
+++ helpers.py
@@ -1,11 +1,10 @@
-import os
+import logging, os
 
-
-from .models import User
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 CSS_FILE = os.path.join(BASE_DIR, 'static', 'datalocker', 'css', 'user_colors.css')
 
+logger = logging.getLogger(__name__)
 
 class UserColorHelper():
     def list_of_avaiable_colors():
@@ -14,12 +13,18 @@ class UserColorHelper():
         defined into a list for use in a session element
         """
         avail_colors = []
-        with open(CSS_FILE, 'r') as cfile:
-            for line in cfile:
-                if line[:1] == '.':
-                    name = line.split()[0][1:]
-                    avail_colors.append(name)
+        try:
+            with open(CSS_FILE, 'r') as cfile:
+                for line in cfile:
+                    if line[:1] == '.':
+                        name = line.split()[0][1:]
+                        avail_colors.append(name)
+        except:
+            logger.warning("The css file for user colors doesn't exist at " \
+            "the following location '%s'" % CSS_FILE)
         return avail_colors
 
     def color_user_lookup():
+        avail_colors = list_of_avaiable_colors()
+        users = []
         return ""
