15209d3e5ed07fbd960016c5de030eef6195286e
Paul Rentschler <par117@psu.edu>
2023-05-23 13:40:51 -0400

    is_authenticated is now a property

--- datalocker/decorators.py
+++ datalocker/decorators.py
@@ -20,7 +20,7 @@ def login_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME,
     an active user, redirecting to the log-in page if necessary.
     """
     actual_decorator = user_passes_test(
-        lambda u: u.is_authenticated() and u.is_active,
+        lambda u: u.is_authenticated and u.is_active,
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
