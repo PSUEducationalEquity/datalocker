f32faac4a278d7020ff61e78ae8432d54552ce79
Paul Rentschler <par117@psu.edu>
2023-05-23 10:17:38 -0400

    Define the app config

---
+++ datalocker/apps.py
@@ -0,0 +1,17 @@
+from django.apps import AppConfig
+
+
+class DataLockerConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Data Locker'
+    label = 'datalocker'
+
+    def ready(self):
+        from django.http.request import HttpRequest
+        if not hasattr(HttpRequest, 'is_ajax'):
+            HttpRequest.is_ajax = request_is_ajax
+
+        try:
+            import djpennstate.signals.newuserlookup
+        except ImportError:
+            pass
