49a43149a43dc4bc8046552be4d1e21c8573e20d
Paul Rentschler <par117@psu.edu>
2023-09-22 11:07:04 -0400

    Improve the User admin

--- datalocker/admin.py
+++ datalocker/admin.py
@@ -86,7 +86,14 @@ class SubmissionAdmin(admin.ModelAdmin):
 
 
 class UserAdmin(auth_UserAdmin):
-    list_display = [
+    actions = [
+        'user_make_active',
+        'user_make_inactive',
+        'user_make_staff',
+        'user_make_notstaff',
+    ]
+    date_hierarchy = 'last_login'
+    list_display = (
         'username',
         'first_name',
         'last_name',
@@ -94,8 +101,45 @@ class UserAdmin(auth_UserAdmin):
         'is_active',
         'is_staff',
         'is_superuser',
-    ]
+        'last_login',
+    )
     list_display_links = ('username', 'first_name', 'last_name')
+
+    def user_make_active(self, request, queryset):
+        rows_updated = queryset.update(is_active=True)
+        if rows_updated == 1:
+            msg = '1 user was'
+        else:
+            msg = f'{rows_updated} users were'
+        self.message_user(request, f'{msg} successfully marked as active.')
+    user_make_active.short_description = 'Mark selected users as active'
+
+    def user_make_inactive(self, request, queryset):
+        rows_updated = queryset.update(is_active=False)
+        if rows_updated == 1:
+            msg = '1 user was'
+        else:
+            msg = f'{rows_updated} users were'
+        self.message_user(request, f'{msg} successfully marked as inactive.')
+    user_make_inactive.short_description = 'Mark selected users as inactive'
+
+    def user_make_staff(self, request, queryset):
+        rows_updated = queryset.update(is_staff=True)
+        if rows_updated == 1:
+            msg = '1 user was'
+        else:
+            msg = f'{rows_updated} users were'
+        self.message_user(request, f'{msg} successfully marked as staff.')
+    user_make_staff.short_description = 'Mark selected users as staff'
+
+    def user_make_notstaff(self, request, queryset):
+        rows_updated = queryset.update(is_staff=False)
+        if rows_updated == 1:
+            msg = '1 user was'
+        else:
+            msg = f'{rows_updated} users were'
+        self.message_user(request, f'{msg} successfully marked as not staff.')
+    user_make_notstaff.short_description = 'Mark selected users as not staff'
 
 
 admin_site = DataLockerAdminSite(name='datalockeradmin')
